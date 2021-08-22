296f3573a6233d9c51b95210ca708bfc15b3f432
ANANT0908 <87381952+ANANT0908@users.noreply.github.com>
2021-08-22 21:06:25 +0530

    fixed problem

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from '../assets/image/logo.png';
+
+const Header = ()=>{
+    return(
+        <header className="header">
+            <nav>
+                <div className="logo" >
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFeHLWlPsfGWk6ZK6uAWSeerX7LMFDly7b3Q&usqp=CAU" alt="Todolist" ></img>
+                </div>
+            </nav>
+        </header>
+    );
+}
+export default Header;
